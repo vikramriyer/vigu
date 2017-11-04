73c940ea6b8cf25cf130166266f37bca46b42219
gaurav6351 <gaurav.harsola@msystechnologies.com>
2017-11-04 16:51:05 +0530

    Seprating the label column from train data

--- churn.py
+++ churn.py
@@ -1,56 +1,58 @@
 """
 @author: GauravH
 """
+
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.preprocessing import StandardScaler
 
 data = pd.read_csv("c:\\Users\\gauravh\\vigu\\train.csv")
-#data.columns.get_loc("HNW_CATEGORY")
-data=data.dropna(axis=1,how='all')
-data=data.dropna(axis=0,how='all')
-data.drop('city',axis=1,inplace=True)
-data.drop('zip',axis=1,inplace=True)
 
-frac = len(data) * 0.8
-data=data.dropna(thresh=frac, axis=1)
-data.shape
+train_x = data.iloc[:, :-1]
 
-null_columns=data.columns[data.isnull().any()]
-data[null_columns].isnull().sum()
+train_y = data.iloc[:,-1].values
 
-obj=data.select_dtypes(include=[object]).columns
+train_x=train_x.dropna(axis=1,how='all')
+train_x=train_x.dropna(axis=0,how='all')
+train_x.drop('city',axis=1,inplace=True)
+train_x.drop('zip',axis=1,inplace=True)
+
+frac = len(train_x) * 0.7
+train_x=train_x.dropna(thresh=frac, axis=1)
+train_x.shape
+
+null_columns=train_x.columns[train_x.isnull().any()]
+train_x[null_columns].isnull().sum()
+
+obj=train_x.select_dtypes(include=[object]).columns
 obj
 
 lis=[]
 for x in obj:
-    lis.append(data.columns.get_loc(x))
-lis    
+    lis.append(train_x.columns.get_loc(x))
+lis   
 
-for col in data.columns.values:
-    if data[col].dtypes != 'object':
+for col in train_x.columns.values:
+    if train_x[col].dtypes != 'object':
         print(col)
-        data[col].fillna(data[col].mean(), inplace=True)
+        train_x[col].fillna(train_x[col].mean(), inplace=True)
         
-for col in data.columns.values:
-    if data[col].dtypes == 'object':
+for col in train_x.columns.values:
+    if train_x[col].dtypes == 'object':
         print(col)
-        data[col].fillna(data[col].value_counts().index[0], inplace=True)
+        train_x[col].fillna(train_x[col].value_counts().index[0], inplace=True)
         
-null_columns=data.columns[data.isnull().any()]
-data[null_columns].isnull().sum()
+null_columns=train_x.columns[train_x.isnull().any()]
+train_x[null_columns].isnull().sum()
 
 labelencoder = LabelEncoder()
 
-for col in data.columns.values:
-    if data[col].dtypes == 'object':
-        data[col] = labelencoder.fit_transform(data[col])
-
+for col in train_x.columns.values:
+    if train_x[col].dtypes == 'object':
+        train_x[col] = labelencoder.fit_transform(train_x[col])
+        
 onehotencoder = OneHotEncoder(categorical_features=lis)
-data = onehotencoder.fit_transform(data).toarray()
+train_x = onehotencoder.fit_transform(train_x).toarray()
 
 sc_X = StandardScaler()
-data = sc_X.fit_transform(data)
+train_x = sc_X.fit_transform(train_x)
